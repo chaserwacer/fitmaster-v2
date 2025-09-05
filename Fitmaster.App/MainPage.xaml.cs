dc105e96529b7740b439a9fe62ccfcf2e14e4e64
Chase Balmer <chase.balmer@outlook.com>
2025-09-05 14:55:40 -0400

    first commit

---
+++ Fitmaster.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Fitmaster.App;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Fitmaster.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Fitmaster.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
